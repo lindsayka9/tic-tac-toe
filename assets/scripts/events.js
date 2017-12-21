160156c46a45bc36222d4deb66ac0a778c91eb0c
lindsayka9 <kat.lindsay.92@gmail.com>
2017-12-21 14:35:09 -0500

    Add click with input grid
    Add click with input to each box in grid

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,6 +1,7 @@
 const getFormFields = require('../../lib/get-form-fields')
 const ui = require('./ui')
 const api = require('./api')
+// const store = require('./store)')
 // Using your knowledge of jQuery write a function, onSubmitForm, that console
 // logs the input in the input field when "save changes" is clicked
 
@@ -36,8 +37,71 @@ const onSignOut = function (event) {
     .catch(ui.signOutFailure)
 }
 
-const onClick = function () {
-  console.log('hello?')
+const onNewGame = function (event) {
+  event.preventDefault()
+  $('#0').text('')
+  $('#1').text('')
+  $('#2').text('')
+  $('#3').text('')
+  $('#4').text('')
+  $('#5').text('')
+  $('#6').text('')
+  $('#7').text('')
+  $('#8').text('')
+}
+
+const onClick0 = function () {
+  event.preventDefault()
+  console.log(this.id)
+  $('#0').text('something')
+}
+
+const onClick1 = function () {
+  event.preventDefault()
+  console.log(this.id)
+  $('#1').text('wut')
+}
+
+const onClick2 = function () {
+  event.preventDefault()
+  console.log(this.id)
+  $('#2').text('how')
+}
+
+const onClick3 = function () {
+  event.preventDefault()
+  console.log(this.id)
+  $('#3').text('how')
+}
+
+const onClick4 = function () {
+  event.preventDefault()
+  console.log(this.id)
+  $('#4').text('why')
+}
+
+const onClick5 = function () {
+  event.preventDefault()
+  console.log(this.id)
+  $('#5').text('how')
+}
+
+const onClick6 = function () {
+  event.preventDefault()
+  console.log(this.id)
+  $('#6').text('get')
+}
+
+const onClick7 = function () {
+  event.preventDefault()
+  console.log(this.id)
+  $('#7').text('it')
+}
+
+const onClick8 = function () {
+  event.preventDefault()
+  console.log(this.id)
+  $('#8').text('done')
 }
 
 const addHandlers = function () {
@@ -45,15 +109,16 @@ const addHandlers = function () {
   $('#sign-in-form').on('submit', onSignIn)
   $('#change-password-form').on('submit', onChangePassword)
   $('#sign-out').on('submit', onSignOut)
-  $('#0').on('click', onClick)
-  $('#1').on('click', onClick)
-  $('#2').on('click', onClick)
-  $('#3').on('click', onClick)
-  $('#4').on('click', onClick)
-  $('#5').on('click', onClick)
-  $('#6').on('click', onClick)
-  $('#7').on('click', onClick)
-  $('#8').on('click', onClick)
+  $('#0').on('click', onClick0)
+  $('#1').on('click', onClick1)
+  $('#2').on('click', onClick2)
+  $('#3').on('click', onClick3)
+  $('#4').on('click', onClick4)
+  $('#5').on('click', onClick5)
+  $('#6').on('click', onClick6)
+  $('#7').on('click', onClick7)
+  $('#8').on('click', onClick8)
+  $('#new-game-button').on('click', onNewGame)
 }
 
 module.exports = {
