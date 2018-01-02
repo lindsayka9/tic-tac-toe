ed47ca755d0ca2b7b4f8fa09e92755345d8b4a6e
lindsayka9 <kat.lindsay.92@gmail.com>
2018-01-02 09:53:12 -0500

    Add folders for gameboard and profile
    Add folders for gameboard and profile scripts

---
+++ assets/scripts/user-profile/api.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const config = require('../config')
+const store = require('../store')
+
+const changePassword = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/change-password/' + store.user.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const signOut = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-out/' + store.user.id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  changePassword,
+  signOut
+}
