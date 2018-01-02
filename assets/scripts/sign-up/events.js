4185ed358fd161617e20839e1235b34e8ea7fadb
lindsayka9 <kat.lindsay.92@gmail.com>
2018-01-02 11:25:02 -0500

    Remove modals, remake input forms
    Remove modal sign in/etc, create new input forms

--- assets/scripts/sign-up/events.js
+++ assets/scripts/sign-up/events.js
@@ -1,7 +1,7 @@
 'use strict'
 const getFormFields = require('../../../lib/get-form-fields')
-const ui = require('../ui')
-const api = require('../api')
+const ui = require('./ui')
+const api = require('./api')
 
 const onSignUp = function (event) {
   event.preventDefault()
