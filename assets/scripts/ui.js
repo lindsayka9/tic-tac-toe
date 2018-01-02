39dd80e983e0d04e764bd53f797877c230858d54
lindsayka9 <kat.lindsay.92@gmail.com>
2018-01-02 09:13:27 -0500

    Remove individual events, create endGame functions
    Remove individual click events, create end of game functions

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -63,6 +63,46 @@ const signOutFailure = function (data) {
   $('#message').text('Error signing out').css('color', 'red')
 }
 
+const newGameSuccess = function (data) {
+  $('#message').text('Successfully saved game').css('color', 'green')
+  store.game = data.game
+  console.log(data)
+}
+
+const newGameFailure = function (data) {
+  $('#message').text('Error creating new game').css('color', 'red')
+}
+
+const showGameSuccess = function (data) {
+  const gameHtml = (
+    `<ul>
+    <h3>Game:</h3>
+      <li>ID: ${data.game.id}</li>
+    </ul>`
+  )
+  $('#content').append(gameHtml)
+  $('#message').text('Successfully changed password!').css('color', 'green')
+}
+
+const showGameFailure = function (data) {
+  $('#message').text('Error changing password').css('color', 'red')
+}
+
+const showGamesSuccess = function (data) {
+  const gameHtml = (
+    `<ul>
+    <h3>Game:</h3>
+      <li>ID: ${data.game.id}</li>
+    </ul>`
+  )
+  $('#content').append(gameHtml)
+  $('#message').text('Successfully changed password!').css('color', 'green')
+  console.log(data)
+}
+
+const showGamesFailure = function (data) {
+  $('#message').text('Error changing password').css('color', 'red')
+}
 module.exports = {
   signUpSuccess,
   signUpFailure,
@@ -72,6 +112,12 @@ module.exports = {
   changePasswordFailure,
   signOutFailure,
   signOutSuccess,
-  gameArray
+  gameArray,
+  newGameSuccess,
+  newGameFailure,
+  showGameSuccess,
+  showGameFailure,
+  showGamesSuccess,
+  showGamesFailure
   // updateBox
 }
