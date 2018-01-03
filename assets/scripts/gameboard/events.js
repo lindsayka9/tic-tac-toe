3be682a278b56b782c579d45bfe8e4e4c05ded6d
lindsayka9 <kat.lindsay.92@gmail.com>
2018-01-03 10:25:29 -0500

    Try out different box click method
    Try out different box click methods

--- assets/scripts/gameboard/events.js
+++ assets/scripts/gameboard/events.js
@@ -11,44 +11,61 @@ const onStartGame = function () {
 }
 
 // function to make move
-const makeMove = function (move, index) {
-  const updateArray = {
-    game: {
-      cell: {
-        index: index,
-        value: move
-      },
-      over: false
-    }
-  }
-  // add move to api array of game
-  const updateCell = JSON.stringify(updateArray)
-  api.updateGame(updateCell)
-    .then(ui.updateGameSuccess)
-    .catch(ui.updateGameFailure)
-}
+// const makeMove = function (move, index) {
+//   const updateArray = {
+//     game: {
+//       cell: {
+//         index: index,
+//         value: move
+//       },
+//       over: false
+//     }
+//   }
+//   // add move to api array of game
+//   const updateBox = JSON.stringify(updateArray)
+//   api.updateGame(updateBox)
+//     .then(ui.updateGameSuccess)
+//     .catch(ui.updateGameFailure)
+// }
 
 //
-const getData = function (box) {
-  return $(box).data('cell-index')
-}
+// const getData = function (box) {
+//   return $(box).data('cell-index')
+// }
 
-const moveOnBoard = function(boxId) {
-  const htmlX =
-  const htmlO =
-}
+let turn = 0
+const playerOne = 'X'
+const playerTwo = 'O'
+const emptyCell = ''
 
-const boxClick = function () {
-  const boxId = '#' + this.id
-  // if game isn't done, mark board, if it is, tell the player
-  if (store.game.over !== true) {
-    moveOnBoard(boxId)
+const makeAMove = function (event) {
+  event.preventDefault()
+  if ($(this).text() !== emptyCell) {
+    console.log('make a move')
+  } else if (turn % 2 !== 0) {
+    $(this).text(playerOne)
   } else {
-    $('#message').text('Game is over.')
+    $(this).text(playerTwo)
   }
+  turn++
 }
 
-//reset game/store file
+// const moveOnBoard = function(boxId) {
+//   const htmlX =
+//   const htmlO =
+// }
+//
+// const boxClick = function () {
+//   const boxId = '#' + this.id
+//   // if game isn't done, mark board, if it is, tell the player
+//   if (store.game.over !== true) {
+//     moveOnBoard(boxId)
+//   } else {
+//     $('#message').text('Game is over.')
+//   }
+// }
+
+// reset game/store file
 const resetGame = function () {
   store.game = null
   store.turn = null
@@ -56,7 +73,7 @@ const resetGame = function () {
   store.currentIndex = null
   // show user profile
   $('#profile').show()
-  //hide button
+  // hide button
   $('#reset-game').hide()
   // clear all cells
   $('#0').html('')
@@ -72,16 +89,26 @@ const resetGame = function () {
 }
 
 const addHandlers = function () {
-  $('#0').on('click', boxClick)
-  $('#1').on('click', boxClick)
-  $('#2').on('click', boxClick)
-  $('#3').on('click', boxClick)
-  $('#4').on('click', boxClick)
-  $('#5').on('click', boxClick)
-  $('#6').on('click', boxClick)
-  $('#7').on('click', boxClick)
-  $('#8').on('click', boxClick)
+  // $('#0').on('click', boxClick)
+  // $('#1').on('click', boxClick)
+  // $('#2').on('click', boxClick)
+  // $('#3').on('click', boxClick)
+  // $('#4').on('click', boxClick)
+  // $('#5').on('click', boxClick)
+  // $('#6').on('click', boxClick)
+  // $('#7').on('click', boxClick)
+  // $('#8').on('click', boxClick)
+  $('#0').on('click', makeAMove)
+  $('#1').on('click', makeAMove)
+  $('#2').on('click', makeAMove)
+  $('#3').on('click', makeAMove)
+  $('#4').on('click', makeAMove)
+  $('#5').on('click', makeAMove)
+  $('#6').on('click', makeAMove)
+  $('#7').on('click', makeAMove)
+  $('#8').on('click', makeAMove)
   $('#reset-game-button').on('click', resetGame)
+  $('#new-game-button').on('click', onStartGame)
 }
 
 module.exports = {
