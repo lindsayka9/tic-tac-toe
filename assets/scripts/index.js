79c40eb0e7e3afa2dccf539a32b87f3156d029b1
lindsayka9 <kat.lindsay.92@gmail.com>
2018-01-04 18:43:26 -0500

    Finish consolidating files
    Remove auth and gameboard folders, consolidate into scripts folder

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,16 +3,12 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const events = require('./events')
-const gameEvents = require('./gameboard/events')
-const authEvents = require('./auth/events')
 
 $(() => {
   setAPIOrigin(location, config)
 })
 
 $(() => {
-  gameEvents.addHandlers()
-  authEvents.addHandlers()
   events.addHandlers()
 })
 // use require with a reference to bundle the file and use it in this file
