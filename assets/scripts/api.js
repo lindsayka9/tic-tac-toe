08582b70558f4195c124d12932d1f418356ff0fa
lindsayka9 <kat.lindsay.92@gmail.com>
2018-01-04 18:42:11 -0500

    Finish UI
    Finish UI

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -1,95 +1,86 @@
-// 'use strict'
-//
-// const config = require('./config')
-// const store = require('./store')
-//
-// // const signUp = function (data) {
-// //   return $.ajax({
-// //     url: config.apiOrigin + '/sign-up',
-// //     method: 'POST',
-// //     data
-// //   })
-// // }
-//
-// // const changePassword = function (data) {
-// //   return $.ajax({
-// //     url: config.apiOrigin + '/change-password/' + store.user.id,
-// //     method: 'PATCH',
-// //     headers: {
-// //       Authorization: 'Token token=' + store.user.token
-// //     },
-// //     data
-// //   })
-// // }
-// //
-// // const signOut = function (data) {
-// //   return $.ajax({
-// //     url: config.apiOrigin + '/sign-out/' + store.user.id,
-// //     method: 'DELETE',
-// //     headers: {
-// //       Authorization: 'Token token=' + store.user.token
-// //     }
-// //   })
-// // }
-//
-// // const newGame = function () {
-// //   return $.ajax({
-// //     url: config.apiOrigin + '/games',
-// //     method: 'POST',
-// //     headers: {
-// //       Authorization: 'Token token=' + store.user.token
-// //     }
-// //   })
-// // }
-//
-// const showGames = function () {
-//   return $.ajax({
-//     url: config.apiOrigin + '/games',
-//     method: 'GET',
-//     headers: {
-//       Authorization: 'Token token=' + store.user.token
-//     }
-//   })
-// }
-//
-// // const updateGame = function (id) {
-// //   return $.ajax({
-// //     url: config.apiOrigin + '/games/' + store.game.id,
-// //     method: 'PATCH',
-// //     headers: {
-// //       Authorization: 'Token token=' + store.user.token
-// //     }
-// //   })
-// // }
-//
-// const watchGame = function (id) {
-//   return $.ajax({
-//     url: config.apiOrigin + '/games/' + store.game.id + 'watch',
-//     method: 'GET',
-//     headers: {
-//       Authorization: 'Token token=' + store.user.token
-//     }
-//   })
-// }
-//
-// const showGame = function (id) {
-//   return $.ajax({
-//     url: config.apiOrigin + '/games' + store.game.id,
-//     method: 'GET',
-//     headers: {
-//       Authorization: 'Token token=' + store.user.token
-//     }
-//   })
-// }
-//
-// module.exports = {
-//   // signUp,
-//   // // signIn,
-//   // changePassword,
-//   // signOut,
-//   showGames,
-//   // newGame,
-//   // updateGame,
-//   watchGame,
-//   showGame
-// }
+'use strict'
+
+const config = require('./config')
+const store = require('./store')
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-in',
+    method: 'POST',
+    data
+  })
+}
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-up',
+    method: 'POST',
+    data
+  })
+}
+
+const changePassword = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/change-password/' + store.user.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const signOut = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-out/' + store.user.id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const newGame = function () {
+  return $.ajax({
+    url: config.apiOrigin + '/games',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const updateGame = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/games/' + store.game.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token,
+      'Content-Type': 'application/json'
+    },
+    data
+  })
+}
+
+const showGameOver = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/games?over=true',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token,
+      'Content-Type': 'application/json'
+    },
+    data
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut,
+  updateGame,
+  newGame,
+  showGameOver
+}
